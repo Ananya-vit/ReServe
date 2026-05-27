f91ddd9043a671da66e1ce359a027ddd7948b823
Ananya-vit <ananyaavit@gmail.com>
2026-05-28 00:53:38 +0530

    initialized express and react also setup env

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
-
+import myEnv from './config/env.js';
 const app = express();
 
+const PORT = myEnv.PORT;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
