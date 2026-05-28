91390e9d681b0f37a64c224b83d510078d57c0fa
Ananya-vit <ananyaavit@gmail.com>
2026-05-28 12:49:27 +0530

    shifte to ts

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
